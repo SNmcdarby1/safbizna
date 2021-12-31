6efe7719713a9f08990f7afd5151d0ab91500b00
SNmcdarby1 <sngereso@gmail.com>
2021-12-31 10:19:14 +0000

    add static folder

--- environ.py
+++ environ.py
@@ -4,3 +4,10 @@ os.environ.setdefault("STRIPE_PUBLIC_KEY", "")
 os.environ.setdefault("STRIPE_SECRET_KEY",""),
 
 os.environ.setdefault("DATABASE_URL",""),
+
+os.environ.setdefault("USE_AWS",""),
+
+os.environ.setdefault('DEVELOPMENT'),
+
+
+os.environ.setdefault('SECRET_KEY'),
