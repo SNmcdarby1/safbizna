e04084f92110ecff42a02bb98fd2d5c1661c7b02
SNmcdarby1 <sngereso@gmail.com>
2022-01-01 15:53:52 +0000

    change debug to false

--- safbizna/settings.py
+++ safbizna/settings.py
@@ -15,6 +15,7 @@ import dj_database_url
 
 from pathlib import Path
 
+import safbizna_env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
---
+++ safbizna_env.py
@@ -0,0 +1,14 @@
+import os
+
+os.environ.get('DATABASE_URL'),
+os.environ.get("STRIPE_PUBLIC_KEY", "")
+os.environ.get("STRIPE_SECRET_KEY",""),
+
+os.environ.get("DATABASE_URL",""),
+
+os.environ.get("USE_AWS",""),
+
+os.environ.get('DEVELOPMENT', ""),
+
+
+os.environ.get('SECRET_KEY',""),
